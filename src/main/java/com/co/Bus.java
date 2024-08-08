b17aff83c81f98acc4d501b277525f0f76bcc0fe
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-08-07 22:37:55 -0500

    Iniciación html y css

--- src/main/java/com/co/Bus.java
+++ src/main/java/com/co/Bus.java
@@ -1,8 +1,11 @@
 package com.co;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +20,9 @@ public class Bus {
     private String placa;
     private String modelo;
     private List<RutaAsignada> rutasAsignadas = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "busesAsignados")
+    private Set<Conductor> conductorAsignado = new HashSet<>();
 
 
 }
--- src/main/java/com/co/Conductor.java
+++ src/main/java/com/co/Conductor.java
@@ -1,8 +1,11 @@
 package com.co;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +16,22 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Conductor {
-    private Integer idConductor;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String cedula;
     private String nombre;
     private String telefono;
     private String direccion;
-    private  List <BusAsignado> busesAsignados = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(
+            name = "Bus_Asignado",
+            joinColumns = @JoinColumn(name = "idConductor"),
+            inverseJoinColumns = @JoinColumn(name = "idBus")
+    )
+    private Set<Bus> busesAsignados = new HashSet<>();
 
 }
