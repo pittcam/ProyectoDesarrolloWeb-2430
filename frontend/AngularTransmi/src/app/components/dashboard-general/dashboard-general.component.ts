da6b97bd2981f7dd791aa14df17ddaba2e39604d
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-09-26 17:06:09 -0500

    dashboard_general_lista conductores

--- frontend/AngularTransmi/src/app/components/dashboard-general/dashboard-general.component.ts
+++ frontend/AngularTransmi/src/app/components/dashboard-general/dashboard-general.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface Conductor {
+  nombre: string;
+  cedula: string;
+  telefono: string;
+  direccion: string;
+}
+
 @Component({
   selector: 'app-dashboard-general',
   standalone: true,
@@ -8,5 +15,20 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-general.component.css'
 })
 export class DashboardGeneralComponent {
+  conductores: Conductor[] = [
+    {
+      nombre: 'Conductor 1',
+      cedula: '123456',
+      telefono: '1234567890',
+      direccion: 'Dirección 1'
+    },
+    {
+      nombre: 'Conductor 2',
+      cedula: '789012',
+      telefono: '0987654321',
+      direccion: 'Dirección 2'
+    },
+    // ...
+  ];
 
 }
---
+++ frontend/AngularTransmi/src/app/components/menu-general/menu-general.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-general',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-general.component.html',
+  styleUrl: './menu-general.component.css'
+})
+export class MenuGeneralComponent {
+
+}
