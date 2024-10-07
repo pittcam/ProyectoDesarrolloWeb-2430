945ec097b6437ee4c106d86de5fe97d5ec329f22
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-10-07 17:21:24 -0500

    Cambio 2

--- frontend/AngularTransmi/src/app/shared/conductor.service.ts
+++ frontend/AngularTransmi/src/app/shared/conductor.service.ts
@@ -2,28 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ConductorDTO } from '../dto/conductor-dto';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConductorService {
 
-  private apiUrl = '/api/conductor';  // URL base del backend
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    })
+  };
 
   constructor(private http: HttpClient) { }
 
   // Obtener lista de todos los conductores
   conductorList(): Observable<ConductorDTO[]> {
-    return this.http.get<ConductorDTO[]>(`${this.apiUrl}`);
+    return this.http.get<ConductorDTO[]>(`${environment.SERVER_URL}/conductor`);
   }
 
   // Buscar conductores por nombre
   buscarPorNombre(nombre: string): Observable<ConductorDTO[]> {
-    return this.http.get<ConductorDTO[]>(`${this.apiUrl}/search?searchText=${nombre}`);
+    return this.http.get<ConductorDTO[]>(`${environment.SERVER_URL}/search?searchText=${nombre}`);
   }
 
   // Eliminar conductor por ID
   eliminarConductor(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${environment.SERVER_URL}/${id}`);
   }
 }
